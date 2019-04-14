484c67eb315febbf35c9770a2222aca029130617
Anton Delitsch <anton.delitsch@implema.se>
2019-04-14 21:44:42 +0200

    Initial version based on Windows Scripts

---
+++ SAP_ABAP_Reset.ps1
@@ -0,0 +1,114 @@
+<#
+.SYNOPSIS
+Connect to a SAP host using the supplied Privileged Account Credentials, and change the password for a local account.
+#>
+function Set-SAPPassword
+{
+	[CmdletBinding()]
+	param (
+		[String]$HostName,
+		[String]$UserName,
+        [String]$OldPassword,
+		[String]$NewPassword,
+		[String]$PrivilegedAccountUserName,
+		[String]$PrivilegedAccountPassword,
+        [String]$Client,
+        [String]$Instance,
+        [String]$SID
+	)
+	
+	try
+	{
+		#Establish the PowerShell Credentials used to execute the script block - based on the Privileged Account Credentials selected for this script	
+        if ($PrivilegedAccountUserName -eq '') {
+            $CredPassword = $PrivilegedAccountPassword
+            $PSUsername = $PrivilegedAccountUserName
+        }
+        else {
+            $CredPassword = $OldPassword
+            $PSUsername = $UserName
+        }
+
+		   $Path = "C:\Program Files\SAP\SAP_DotNetConnector3_Net40_x64\"
+
+          [String]$File = $Path + "sapnco.dll"
+          Add-Type -Path $File
+          [String]$File = $Path + "sapnco_utils.dll"
+          Add-Type -Path $File
+ 
+            $cfgParams = New-Object SAP.Middleware.Connector.RfcConfigParameters
+            $cfgParams.Add($cfgParams::Name, $SID)
+            $cfgParams.Add($cfgParams::AppServerHost, $HostName)
+            $cfgParams.Add($cfgParams::SystemNumber, $Instance)
+            $cfgParams.Add($cfgParams::Client, $Client)
+            $cfgParams.Add($cfgParams::User, $PSUserName)
+
+            $cfgParams.Add($cfgParams::Password, $CredPassword)
+            $destination = [SAP.Middleware.Connector.RfcDestinationManager]::GetDestination($cfgParams)
+		
+		Try {
+                      #-Metadata----------------------------------------------------
+              $rfcFunction = $destination.Repository.CreateFunction("ME_USER_CHANGE_PASSWORD")
+              $rfcFunction.SetValue("USERNAME", $UserName)
+              $rfcFunction.SetValue("PASSWORD", $OldPassword)
+              $rfcFunction.SetValue("NEW_PASSWORD", $NewPassword)
+              #$rfcFunction.SetValue("USE_BAPI_RETURN", "1")
+
+              $rfcFunction.Invoke($destination)
+              
+              if ($rfcFunction.GetValue("SUCCESSFUL") -eq "X") {
+              $resultsarray = "Success"
+              } else {
+              $resultsarray = $rfcFunction.GetValue("ERROR_MESSAGE")
+              }
+              
+            }
+            Catch {
+              $resultsarray = $_.Exception.Message
+            }		
+		#Execute the command and put the output in an array.
+		$resultsarray = Invoke-Command -SessionOption $PSSessionOption -ComputerName $HostName -Authentication 'Default' -Credential $Credentials -ScriptBlock $scriptBlock -ArgumentList $HostName, $UserName, $NewPassword 2>&1 #Using 2>&1 to ensure STDERR is piped to STDOUT
+
+		#include Port Number in SPN if required
+		If ($resultsarray.ErrorDetails.Message -like "*Connecting to remote server $HostName failed with the following error message*0x80090322*" -or $resultsarray.ErrorDetails.Message -like "*Connecting to remote server $HostName failed with the following error message*The WS-Management service cannot complete the operation within the time specified in OperationTimeout*")
+		{
+			#Execute the command and put the output in an array.
+			$PSSessionOption = New-PSSessionOption -OperationTimeout [OperationTimeout] -OpenTimeOut [OpenTimeout] -IncludePortInSPN
+		    $resultsarray = Invoke-Command -SessionOption $PSSessionOption -ComputerName $HostName -Authentication 'Default' -Credential $Credentials -ScriptBlock $scriptBlock -ArgumentList $HostName, $UserName, $NewPassword 2>&1 #Using 2>&1 to ensure STDERR is piped to STDOUT
+		}
+		
+		if ($resultsarray -eq "Success")
+		{
+			Write-Output "Success"
+		}
+		else
+		{
+			switch -wildcard ($resultsarray.ToString().ToLower())
+			{
+				"*Access is denied*" { Write-Output "Failed to reset the local SAP account '$UserName' on Host '$HostName' due to the error 'Access is Denied. Please refer to Microsoft's documentation about 'about_Remote_Troubleshooting', in particular the 'LocalAccountTokenFilterPolicy' registry key if remoting in with a Local Administrator's account."; break }
+				"*WinRM cannot complete the operation*" { Write-Output "Failed to reset the local Windows password for account '$UserName' on Host '$HostName' as it appears the Host is not online, or PowerShell Remoting is not enabled."; break }
+				"*WS-Management service running*" { Write-Output "Failed to reset the local Windows password for account '$UserName' on Host '$HostName' as it appears the Host is not online, or PowerShell Remoting is not enabled."; break }
+				"*cannot find the computer*" { Write-Output "Failed to reset the local Windows password for account '$UserName' on Host '$HostName' as it appears the Host is not online, or PowerShell Remoting is not enabled."; break }
+				"*no logon servers available*" { Write-Output "Failed to reset the local Windows password for account '$UserName' on Host '$HostName'. There are currently no logon servers available to service the logon request."; break }
+				"*currently locked*" { Write-Output "Failed to reset the local password for account '$UserName' on Host '$HostName'. The referenced account is currently locked out and may not be logged on to."; break }
+				"*user name or password is incorrect*" { Write-Output "Failed to reset the local password for account '$UserName' on Host '$HostName' as the Privileged Account password appears to be incorrect, or the account is currently locked."; break }
+				"*username does not exist*" { Write-Output "Failed to reset the local password for account '$UserName' on Host '$HostName' as the UserName does not exist."; break }
+				#Add other wildcard matches here as required
+				default { Write-Output "Failed to reset the local password for account '$UserName' on Host '$HostName'.Error = $resultsarray." }
+			}
+		}
+	}
+	catch
+	{
+		switch -wildcard ($error[0].Exception.ToString().ToLower())
+		{
+			"*The user name or password is incorrect*" { Write-Output "Failed to connect to the Host '$HostName' to reset the password for the account '$UserName'. Please check the Privileged Account Credentials provided are correct."; break }
+			"*cannot bind argument to parameter*" { Write-Output "Failed to reset the local password for account '$UserName' on Host '$HostName' as it appears you may not have associated a Privileged Account Credential on the Reset Options tab for the Password record."; break }
+			#Add other wildcard matches here as required
+			default { Write-Output "Failed to reset the local Windows password for account '$UserName' on Host '$HostName'. Error = " $error[0].Exception }
+		}
+	}
+}
+
+#Make a call to the Set-SAPPassword function
+Set-SAPPassword -HostName '[HostName]' -UserName '[UserName]' -OldPassword '[OldPassword]' -NewPassword '[NewPassword]' -PrivilegedAccountUserName '[PrivilegedAccountUserName]' -PrivilegedAccountPassword '[PrivilegedAccountPassword]' -Client '[GenericField1]' -Instance '[GenericField2]' -SID '[GenericField3]'
---
+++ SAP_ABAP_Validate.ps1
@@ -0,0 +1,84 @@
+<#
+.SYNOPSIS
+Connect to a SAP host and validate the password for the account is correct by using RFC.
+#>
+function Validate-SAPPassword
+{
+	[CmdletBinding()]
+	param (
+		[String]$HostName,
+		[String]$UserName,
+		[String]$CurrentPassword,
+        [String]$PrivilegedAccountUserName,
+        [String]$Client,
+        [String]$Instance,
+        [String]$SID
+       	)
+	
+	try
+	{
+		   $Path = "C:\Program Files\SAP\SAP_DotNetConnector3_Net40_x64\"
+
+          [String]$File = $Path + "sapnco.dll"
+          Add-Type -Path $File
+          [String]$File = $Path + "sapnco_utils.dll"
+          Add-Type -Path $File
+ 
+            $cfgParams = New-Object SAP.Middleware.Connector.RfcConfigParameters
+            $cfgParams.Add($cfgParams::Name, $SID)
+            $cfgParams.Add($cfgParams::AppServerHost, $HostName)
+            $cfgParams.Add($cfgParams::SystemNumber, $Instance)
+            $cfgParams.Add($cfgParams::Client, $Client)
+            $cfgParams.Add($cfgParams::User, $UserName)
+
+            $cfgParams.Add($cfgParams::Password, $CurrentPassword)
+            $destination = [SAP.Middleware.Connector.RfcDestinationManager]::GetDestination($cfgParams)
+		
+		Try {
+                      #-Metadata----------------------------------------------------
+            $rfcFunction = $destination.Repository.CreateFunction("RFC_PING")
+            $destination.Ping()
+            $resultsarray = "Ping successful"
+            
+            }
+            Catch {
+              $resultsarray = $_.Exception.Message
+            }
+        
+		switch -wildcard ($resultsarray.ToString().ToLower())
+		{
+			"*Host Offline*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the Host is not online, or a firewall is blocking port 135 and 445."; break }
+			"*You cannot call a method on a null-valued expression*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - UserName or Password is incorrect."; break } #TODOD
+            "*Access is denied*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the account may be locked out."; break }
+			#Add other wildcard matches here as required
+            default { Write-Output "Success" }
+		}
+	}
+	catch
+	{
+		switch -wildcard ($error[0].Exception.ToString().ToLower())
+		{
+            "*The network path was not found*" { Write-Output "Success"; break } #Can return this value if the password is correct for PowerShell 5. If incorrect, it will return a different value
+            "*disabled*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the account is disabled."; break }
+			"*No such host is known*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the Host is not online or no longer exists."; break }
+            "*locked out*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the account may be locked out."; break }
+            "*an extended error*" { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - UserName or Password is incorrect."; break }
+            #Using ValidateCredentials can still cause access denied because of UAC, so we need to allow for it
+            "*Access is denied*" 
+            { 
+                if ($error[0].Exception.ToString() -like '*System.DirectoryServices.AccountManagement.PrincipalOperationException*') 
+                { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName' - it appears the account is disabled or locked out, or Username or Password is incorrect."; break }
+                else
+                { Write-Output "Success"; break }
+            } 
+
+			#Add other wildcard matches here as required
+			default { Write-Output "Failed to validate the local password for account '$UserName' on Host '$HostName'. Error = " $error[0].Exception }
+		}
+	}
+	finally {  }
+}
+
+
+#Make a call to the Validate-SAPPassword function
+Validate-SAPPassword -HostName '[HostName]' -UserName '[UserName]' -CurrentPassword '[CurrentPassword]' -Client '[GenericField1]' -Instance '[GenericField2]' -SID '[GenericField3]'
